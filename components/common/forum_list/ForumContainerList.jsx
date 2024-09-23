a58ca385e6a9ee3b71a87fc1d89cffe2291d97c2
ghtjd1358 <hojjang18@naver.com>
2024-09-23 15:36:34 +0900

    내용 정리 및 주석

--- components/common/forum_list/ForumContainerList.jsx
+++ components/common/forum_list/ForumContainerList.jsx
@@ -25,6 +25,7 @@ export default function ForumContainerList() {
         { skip: !searchQuery }
     );
 
+    // 검색
     useEffect(() => {
         if (!searchQuery) {
             dispatch(fetchRead());
